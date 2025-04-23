97705292abaa3dd99c093f3b2f04c91f04a39de6
anandakrishnan <automasi66@gmail.com>
2025-04-24 01:45:52 +0530

    add handelclick to update the sample onclick

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,30 @@
 import './App.css'
 import { useState } from 'react'
 
+
+
+
+
+
+
+
+
 function App() {
-  const sample=useState('hai')
+
+
+  const [sample,setSample]=useState('hai')
   console.log(sample)
+
+
+  function handelClick(){
+    setSample('you won')
+  }
 
   return (
     <>
         <div className='box'>
        <h1>hai</h1>
-
-       <p>this is {sample[0]}</p>
+       <button onClick={handelClick}>{sample}</button>
         </div>
     </>
   )
