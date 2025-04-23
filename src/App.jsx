1d8c5da8b04c16aa2f2d95acdeda5c4c35a78f75
anandakrishnan <automasi66@gmail.com>
2025-04-24 01:32:00 +0530

    using usedtate for frist time with only the intial value

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 
 
 import './App.css'
-
+import { useState } from 'react'
 
 function App() {
-  
+  const sample=useState('hai')
+  console.log(sample)
 
   return (
     <>
         <div className='box'>
        <h1>hai</h1>
+
+       <p>this is {sample[0]}</p>
         </div>
     </>
   )
