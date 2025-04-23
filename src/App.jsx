06df26ce76490bba4d2f9620d0d8cfc9aebd9603
anandakrishnan <automasi66@gmail.com>
2025-04-24 03:43:27 +0530

    made a simple state that gives opposite boolen value of its initial value like if click button it become yes and again click that become no

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,18 @@ import './App.css'
 import { useState } from 'react'
 
 function App() {
-let [count,setCount]=useState(0)
+let [goout,setGoout]=useState(false)
 
 function handleClick(){
-setCount(count+1)
+ setGoout(!goout)
 }  
 
-function handleClickminus(){
-  setCount(perv=>
-     perv-1
-  )
-}
+
   return (
     <>
         <div className='box'>
        <h1>hai</h1>
-        <h2>{count}</h2> 
-       <button onClick={handleClick}>+</button>
-       <button onClick={handleClickminus}>-</button>
+       <button onClick={handleClick}>click {goout ? "yes":"no"}</button>
         </div>
     </>
   )
