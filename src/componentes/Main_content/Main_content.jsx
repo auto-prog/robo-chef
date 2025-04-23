01c738280cb3c4ffde4a280db7ff754cd8fb387c
anandakrishnan <automasi66@gmail.com>
2025-04-24 05:10:38 +0530

    when user enter a value it saves in a array and also display as list

--- src/componentes/Main_content/Main_content.jsx
+++ src/componentes/Main_content/Main_content.jsx
@@ -1,26 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Main_conten.css"
+
 function Main_content() {
+  let ingredients_list = ['apple', 'banana', 'orange', 'mango'];
+  const [ingredient,setingredient]=useState(ingredients_list)
 
 
-let ingredients_list=["apple","chicken","banana"]
-const each=ingredients_list.map((food)=>{
-  return <li key={food}>{food}</li>
+let  each =ingredient.map((food,index)=>{
+  return <li key={index}>{food}</li>
+  
  })
+ console.log(ingredient)
 
 
+
+ 
 function handelSubmit(e){
+
   e.preventDefault()
-  const ingredients= new FormData(e.currentTarget).get("ingredient")
-   ingredients_list=[...ingredients_list,ingredients]
-  return console.log(ingredients_list)
+  
+  let inputvalue= new FormData(e.currentTarget).get("inputvalue")
+  inputvalue ? setingredient( prev=> [...prev,inputvalue]) : window.alert('Pls enter any one ingredient !')
+  
    
 
 }
   return (
     <main>
     <form className='add-ingredients' onSubmit={handelSubmit} >
-        <input type="text" aria-label='this is a input' placeholder='Eg: eggs' name='ingredient'/>
+        <input type="text" aria-label='this is a input' placeholder='Eg: eggs' name='inputvalue'/>
         <button type='submit'>Add Ingredients</button>
     </form>
       {each}
