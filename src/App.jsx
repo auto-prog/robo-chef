6e3d42763a0f907fd4f6af9c135d36ecf72f8c5f
anandakrishnan <automasi66@gmail.com>
2025-04-24 02:39:41 +0530

    made a simple counter system using usestate , using arrow function to pass argument to the function , that decide weather it to be increment or decrement

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,21 @@
 import './App.css'
 import { useState } from 'react'
 
-
-
-
-
-
-
-
-
 function App() {
+let [count,setCount]=useState(0)
 
+function handleClick(state){
 
-  const [sample,setSample]=useState('hai')
-  console.log(sample)
+state ? setCount(count+1):setCount(count-1)
 
-
-  function handelClick(){
-    setSample('you won')
-  }
-
+}  
   return (
     <>
         <div className='box'>
        <h1>hai</h1>
-       <button onClick={handelClick}>{sample}</button>
+        <h2>{count}</h2> 
+       <button onClick={() => handleClick(false)}>-</button>
+       <button onClick={() => handleClick(true)}>+</button>
         </div>
     </>
   )
