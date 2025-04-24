b82f3b743820aeabfd7b7ad60ee91a2ee08c62b9
anandakrishnan <automasi66@gmail.com>
2025-04-24 12:09:04 +0530

    added name to input foe email and password

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,10 @@ function App() {
         <div className='box'>
          <form >
             <label htmlFor='email'>Email</label>
-            <input id='email' type="email" placeholder='example12@gmail.com' />
+            <input id='email' type="email" placeholder='example12@gmail.com' name='email' />
             <br/>
             <label htmlFor='password'>Password</label>
-            <input id='password' type="password" placeholder='password@123' />
+            <input id='password' type="password" placeholder='password@123' name='password'/>
          </form>
         </div>
     </>
