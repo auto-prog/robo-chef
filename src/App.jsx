55cf6e7d6f8fada9c48c7af713c43e7ebfd49416
anandakrishnan <automasi66@gmail.com>
2025-04-24 12:41:24 +0530

    consol log email , pasword that user enter in mordern way that use action event lisener ,which tacils over every repeating line of code like event.preventDefault(),formel.reset() etc.. which need to take values user enter in the input feild of form

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,23 @@ import './App.css'
 
 
 function App() {
-  
 
+  function signup(formdata){
+    const email=formdata.get('email')
+    const password = formdata.get('password')
+    console.log(email)
+    console.log(password)
+  }
   return (
     <>
         <div className='box'>
-         <form >
+         <form action={signup}>
             <label htmlFor='email'>Email</label>
             <input id='email' type="email" placeholder='example12@gmail.com' name='email' />
             <br/>
             <label htmlFor='password'>Password</label>
             <input id='password' type="password" placeholder='password@123' name='password'/>
+            <button type='submit'>submit</button>
          </form>
         </div>
     </>
