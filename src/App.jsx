48ce4b55ffe9cdf084b30df9b23165e61d1683a7
anandakrishnan <automasi66@gmail.com>
2025-04-24 13:19:47 +0530

    give the radio a value by the value  method of giving value to the radio and also give a defalut checked method that in part-time , which checked by default

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,51 @@ function App() {
   function signup(formdata){
     const email=formdata.get('email')
     const password = formdata.get('password')
-    console.log(email)
+    const employmentStatus=formdata.get("employmentStatus")
+    console.log(employmentStatus)
     console.log(password)
+
   }
   return (
     <>
-        <div className='box'>
-         <form action={signup}>
-            <label htmlFor='email'>Email</label>
-            <input id='email' type="email" placeholder='example12@gmail.com' name='email' />
-            <br/>
-            <label htmlFor='password'>Password</label>
-            <input id='password' type="password" placeholder='password@123' name='password'/>
-            <button type='submit'>submit</button>
-         </form>
+    <div className='box'>
+        
+    <section>
+      <h1>Signup form</h1>
+      <form action={signup}>
+
+        <label htmlFor="email">Email:</label>
+        <input id="email" defaultValue="joe@schmoe.com" type="email" name="email" placeholder="joe@schmoe.com" />
+
+        <label htmlFor="password">Password:</label>
+        <input id="password" defaultValue="password123" type="password" name="password" />
+
+        <label htmlFor="description">Description:</label>
+        <textarea id="description" name="description" defaultValue="This is a description"></textarea>
+
+        <fieldset>
+          <legend>Employment Status:</legend>
+          <label>
+            <input type="radio" name="employmentStatus" value="Unemployed" />
+            Unemployed
+        </label>
+          <label>
+            <input type="radio" name="employmentStatus" defaultChecked={true} value="Part-time" />
+            Part-time
+        </label>
+          <label>
+            <input type="radio" name="employmentStatus" value="full-time"/>
+            Full-time
+        </label>
+        </fieldset>
+
+
+
+        <button>Submit</button>
+
+      </form>
+    </section>
+  
         </div>
     </>
   )
