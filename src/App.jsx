d92a5799a24ce9f6d1737a8505fcf21d42d66f78
anandakrishnan <automasi66@gmail.com>
2025-04-24 12:06:41 +0530

    added the password input with label

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,9 @@ function App() {
          <form >
             <label htmlFor='email'>Email</label>
             <input id='email' type="email" placeholder='example12@gmail.com' />
+            <br/>
+            <label htmlFor='password'>Password</label>
+            <input id='password' type="password" placeholder='password@123' />
          </form>
         </div>
     </>
